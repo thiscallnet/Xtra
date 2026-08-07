e0f921991a73a49613e910a28be1dc36a99f2b15
coolguy124 <55761090+coolguy124@users.noreply.github.com>
2026-08-07 11:50:41 +0200

    Add modern AMOLED theme

--- app/src/main/java/com/github/andreyasadchy/xtra/util/ContextExtensions.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/util/ContextExtensions.kt
@@ -41,7 +41,9 @@ fun Activity.applyTheme() {
     } else {
         prefs().getString(C.THEME, "0") ?: "0"
     }
-    if (prefs().getBoolean(C.UI_THEME_MATERIAL3, true)) {
+    if (prefs().getBoolean(C.UI_THEME_MATERIAL3, true) && (theme == "7" || theme == "8")) {
+        setTheme(if (theme == "8") R.style.ModernAmoledTheme else R.style.ModernTheme)
+    } else if (prefs().getBoolean(C.UI_THEME_MATERIAL3, true)) {
         val reducedPadding = prefs().getBoolean(C.UI_THEME_REDUCED_PADDING, false)
         val compactText = prefs().getBoolean(C.UI_THEME_COMPACT_TEXT, false)
         when (prefs().getString(C.UI_THEME_ROUNDED_CORNERS, "0")) {
